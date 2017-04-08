7b24b82fc04be427505d5c0ef68a69638afe40a9
jorkanofaln <jorgomkone@gmail.com>
2017-04-08 13:05:42 -0400

    Add files via upload

--- jeu_graphique.py
+++ jeu_graphique.py
@@ -9,24 +9,25 @@ def partie_graphique():
     pass
     #Partie.joueurs.append(nom_joueur1.get(),nom_joueur2.get(),nom_joueur3.get())
     #Partie.jouer_partie(Partie)
-    fenetre = Toplevel()
+    fenetre = Tk()
     joueur=Label(fenetre,text="Test").grid(row=1,column=1)
     nb_tours_restant = Label(fenetre,text="2 tours restants").grid(row=1,column=8)
-    lancer_des = Button(fenetre,text="Lancer dés",command=Combinaison._lancer_des(Combinaison,5)).grid(row=4,column=6)
+    relancer_des = Button(fenetre,text="Lancer dés",command=Combinaison.relancer_des(Combinaison,Combinaison.index_a_relancer)).grid(row=4,column=6)
     terminer_tour = Button(fenetre,text="Terminer").grid(row=4,column=7)
-    sauvegarde = Button(fenetre,text="Sauvegarde").grid(row=9,column=3)
-    de_1= Button(fenetre,text="De1",command=Combinaison.ajouter_des_a_index(Combinaison,1,Combinaison.index_a_relancer)).grid(row=2,column=1)
-    de_2 = Button(fenetre,text="De2",command=Combinaison.ajouter_des_a_index(Combinaison,2,Combinaison.index_a_relancer)).grid(row=2,column=2)
-    de_3 = Button(fenetre, text="De3",command=Combinaison.ajouter_des_a_index(Combinaison,3,Combinaison.index_a_relancer)).grid(row=2, column=3)
-    de_4 = Button(fenetre, text="De4",command=Combinaison.ajouter_des_a_index(Combinaison,4,Combinaison.index_a_relancer)).grid(row=2, column=4)
-    de_5 = Button(fenetre, text="De5",command=Combinaison.ajouter_des_a_index(Combinaison,5,Combinaison.index_a_relancer)).grid(row=2, column=5)
-    reset = Button(fenetre,text="Redémarrer").grid(row=4,column=8)
+    sauvegarde = Button(fenetre,text="Sauvegarde",command=Partie.sauvegarde).grid(row=9,column=3)
+    #textvariable=Combinaison.des[0]
+    de_1= Button(fenetre,text="0",command=Combinaison.ajouter_des_a_index(Combinaison,1,Combinaison.index_a_relancer)).grid(row=2,column=1)
+    de_2 = Button(fenetre,text="1",command=Combinaison.ajouter_des_a_index(Combinaison,2,Combinaison.index_a_relancer)).grid(row=2,column=2)
+    de_3 = Button(fenetre,text="2",command=Combinaison.ajouter_des_a_index(Combinaison,3,Combinaison.index_a_relancer)).grid(row=2, column=3)
+    de_4 = Button(fenetre, text="3",command=Combinaison.ajouter_des_a_index(Combinaison,4,Combinaison.index_a_relancer)).grid(row=2, column=4)
+    de_5 = Button(fenetre, text="4",command=Combinaison.ajouter_des_a_index(Combinaison,5,Combinaison.index_a_relancer)).grid(row=2, column=5)
+    reset = Button(fenetre,text="Nouvelle Partie").grid(row=4,column=8)
     #fenetre.mainloop()
 def charger_partie():
         try:
-            pass
+            Partie.restaure()
         except:
-            messagebox.showerror("Erreur chargement","Le fichier de suavegarde n'est pas disponible")
+            messagebox.showerror("Erreur chargement","Le fichier de sauvegarde n'est pas disponible")
 def lire_regles():
     messagebox.showinfo("Règles du jeu","Le but du jeu est d'obtenir la combinaison ayant la plus grande valeur."
     "La combinaison valant le plus est le Quinton, soit lui avec 5 figures similaires, le carré avec 4 figures similaires."
@@ -34,7 +35,7 @@ def lire_regles():
     "La séquence avec toutes les figures suivis l'une de l'autre, le double carré avec deux paires de figures "
     "et le carré avec une paire de figures. Chacun des joueurs à le droit 3 lancers sauf si le joueur précédent a"
     "a réussi en moins de coups, donc le nombre de coups réussis est le nouveau total")
-root = Tk()
+root = Toplevel()
 nom_joueur1 = Entry(root,textvariable=nom1).grid(row=2,column=3)
 joueur1= Label(root,text="Joueur 1").grid(row=1,column=3)
 nom_joueur2 = Entry(root,textvariable=nom2).grid(row=2,column=4)
@@ -43,8 +44,8 @@ nom_joueur3 = Entry(root,textvariable=nom3).grid(row=2,column=5)
 joueur3= Label(root,text="Joueur 3").grid(row=1,column=5)
 joker_d_as = False
 Checkbutton(root,text="As en tant que joker",variable = joker_d_as).grid(row=4,column=5)
-commencer_partie = Button(root,text="Commencer",command = partie_graphique()).grid(row=4,column=2)
+commencer_partie = Button(root,text="Commencer",command = partie_graphique).grid(row=4,column=2)
 regles = Button(root,text="Lire règles",command=lire_regles).grid(row=4,column=3)
 quitter = Button(root,text="Quitter",command=root.quit).grid(row=5,column=3)
-restaurer = Button(root,text="Restaurer",command=charger_partie()).grid(row=5,column=2)
+restaurer = Button(root,text="Restaurer",command=charger_partie).grid(row=5,column=2)
 root.mainloop()
