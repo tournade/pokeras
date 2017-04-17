6dc5b524920d6b74f757acc57c8903f9e66b3df1
pascal johnson <Pascal.johnson@kpconcept.com>
2017-04-17 19:35:32 -0400

    derniere ajustement

--- Readme.txt
+++
@@ -1,1 +0,0 @@
-Jeux de poker as
---
+++ main.py
@@ -0,0 +1,7 @@
+from Interface_jeu import mon_interface
+
+
+if __name__ == '__main__':
+    # Instanciation de la fenêtre et démarrage de sa boucle principale.
+    fenetre = mon_interface()
+    fenetre.mainloop()
